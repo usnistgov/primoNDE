a8195d0cf1969928d469086fcd97385b58bfe2a9
MDavisMCD <matthew.c.davis@nist.gov>
2025-11-17 16:32:37 -0500

    updates footer and header. Still need auto flex in about text

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,8 @@
 // Define the map
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
 export const selectorComponentMap = new Map<string, any>([
-
-
-
+  ['nde-header-before', HeaderComponent],
+  ['nde-footer-bottom', FooterComponent],
 ]);
