5bce71b86097378d3e33c882aba550fdbdd86ef2
MDavisMCD <matthew.c.davis@nist.gov>
2026-02-18 13:58:31 -0500

    fixes mapping

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,5 +1,7 @@
 import { NdeServiceNowContainerCustomComponent } from "../nde-service-now-container-custom/nde-service-now-container-custom.component";
 import { NdeCollectionDiscoveryGalleryItemContainerCustomComponent } from "../nde-collection-discovery-gallery-item-container-custom/nde-collection-discovery-gallery-item-container-custom.component";
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
@@ -7,11 +9,6 @@ export const selectorComponentMap = new Map<string, any>([
 
   // Mapping your new collection discovery component
   ['nde-collection-discovery-gallery-item-bottom', NdeCollectionDiscoveryGalleryItemContainerCustomComponent],
-]);
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-
-export const selectorComponentMap = new Map<string, any>([
   ['nde-header-before', HeaderComponent],
   ['nde-footer-bottom', FooterComponent],
 ]);
