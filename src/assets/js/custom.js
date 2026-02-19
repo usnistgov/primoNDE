8a18f6cd0163722fbf2de413da8abffc16c127d4
MDavisMCD <matthew.c.davis@nist.gov>
2026-02-19 14:19:05 -0500

    adds new Google Analytics Tag for NDE

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,1 +1,19 @@
 console.log('Hello from custom.js');
+
+//START --- Google Analytics ---
+//STANDARD CODE with edit to first line from 'async src' to 'googleAnalyticsUrl.src'
+
+  var googleAnalyticsUrl = document.createElement('script');
+  googleAnalyticsUrl.src = "https://www.googletagmanager.com/gtag/js?id=G-HNFWXGE46M";
+  googleAnalyticsUrl.type = 'text/javascript';
+  googleAnalyticsUrl.async = true;
+  document.head.appendChild(googleAnalyticsUrl);
+
+  var googleAnalyticsCode = document.createElement('script');
+  googleAnalyticsCode.innerHTML = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-HNFWXGE46M');`;
+  document.head.appendChild(googleAnalyticsCode);
+
+  //END - Google Analytics ---
